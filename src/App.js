b3be940e0b229e9fab11e5c38374a47e65277a3b
Temitope joshua <ojotemitopeidnoble10@gmail.com>
2022-10-14 00:12:05 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 
+import React, { useRef } from "react";
 import './App.css';
 import './css/default.css'
+import CEO from './img/ceo.jpg';
+import ASS from './img/assit.jpg'
 function App() {
+  const ServicesRef = useRef(null);
+  const Home= useRef(null)
+  const About= useRef(null)
+  const gotoServices = () =>
+    window.scrollTo({
+      top: ServicesRef.current.offsetTop,
+      behavior: "smooth",
+      // You can also assign value "auto"
+      // to the behavior parameter.
+    });
+    const gotoHome = () =>
+    window.scrollTo({
+      top: Home.current.offsetTop,
+      behavior: "smooth",
+      // You can also assign value "auto"
+      // to the behavior parameter.
+    }); 
+    const gotoAbout = () =>
+    window.scrollTo({
+      top: About.current.offsetTop,
+      behavior: "smooth",
+      // You can also assign value "auto"
+      // to the behavior parameter.
+    });
+
   return (
     <div className="container-fluid">
     <section className="smart-scroll">
-  <div className="container-fluid" >
+  <div className="container-fluid" ref={Home}>
     <nav className="navbar navbar-expand-md navbar-dark">
       <a className="navbar-brand heading-black" href='/'>
-        NVPCS
+        NMA
       </a>
       <button className="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span data-feather="grid" />
@@ -16,31 +44,31 @@ function App() {
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link page-scroll" href="/">
+            <div className="nav-link page-scroll a"  onClick={gotoHome}>
              Home
-            </a>
+            </div>
           </li>
           <li className="nav-item">
-            <a className="nav-link page-scroll"  href="/">
+            <div className="nav-link page-scroll a" onClick={gotoAbout} >
            About Us
-            </a>
+            </div>
           </li>
           {/* <li className="nav-item">
             <a className="nav-link page-scroll" href="#faq/">
               FAQ
             </a>
           </li> */}
-          <li className="nav-item">
-            <a className="nav-link page-scroll" href="/">
+          <li className="nav-item" onClick={gotoServices}>
+            <div className="nav-link page-scroll a">
             Contact Us
-            </a>
+            </div>
           </li>
         </ul>
       </div>
     </nav>
   </div>
 </section>
-<section className=" py-md-0 bg-hero" >
+<section className=" py-md-0 bg-hero" ref={About}>
   <div className="container">
     <div className="row vh-md-100">
       <div className="col-md-8 col-sm-10 col-12 mx-auto my-auto text-center">
@@ -114,37 +142,18 @@ function App() {
     <div className="row">
       <div className="col-md-6 mx-auto text-center">
         <h2>Our Goals  &amp; Objectives</h2>
-        <p className="text-muted lead">Answers to most common questions.</p>
+       
       </div>
     </div>
     <div className="row mt-2">
-      <div className="col-md-10 mx-auto">
-        <div className="row">
-          <div className="col-md-6 mb-5">
-            <h6>Can I try it for free?</h6>
-            <p className="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
+      <div className="col-md-12 mx-auto justify" >
+      <div className="col-md-8 mb-5 text-center">
+            {/* <h6>Can I try it for free?</h6> */}
+            <p className="text-muted">To bridge the gap between founders of the start-ups and Venture capital and Angel Networks,
+            Funding raising process for start-up founders is onerous for founders, and many founders complain that it is time consuming, hence, the objective of NMA is to provide a holding-hand for start-up founders to focus on buiding their products while we take care of the administrative process of their fund-raising activities.
+            
+            </p>
           </div>
-          <div className="col-md-6 mb-5">
-            <h6>Do you have hidden fees?</h6>
-            <p className="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
-          </div>
-          <div className="col-md-6 mb-5">
-            <h6>What are the payment methods you accept?</h6>
-            <p className="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
-          </div>
-          <div className="col-md-6 mb-5">
-            <h6>How often do you release updates?</h6>
-            <p className="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
-          </div>
-          <div className="col-md-6 mb-5">
-            <h6>What is your refund policy?</h6>
-            <p className="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
-          </div>
-          <div className="col-md-6 mb-5">
-            <h6>How can I contact you?</h6>
-            <p className="text-muted">Nam liber tempor cum soluta nobis eleifend option congue nihil imper per tem por legere me doming.</p>
-          </div>
-        </div>
       </div>
     </div>
    
@@ -152,60 +161,51 @@ function App() {
 </section>
 
 <div className="container">
-    <div className="row">
+    <div className="row ">
       <div className="col-md-6 mx-auto text-center">
-        <h2 className="heading-black">NVM</h2>
+        <h2 className="heading-black">NMA Members</h2>
    
       </div>
     </div>
-    <div className="row mt-5">
-      <div className="col-md-4">
+    <div className="row mt-5 jus">
+    <div className="col-md-4">
         <div className="card">
-          <a href="/">
-            <img className="card-img-top img-raised" src="img/blog-1.jpg" alt="Blog 1" />
+          <a href="/" >
+            <img className="card-img-top img-raised object" src={CEO} alt="Blog 1" />
           </a>
           <div className="card-body">
-            <a href="/" className="card-title mb-2"><h5>We launch new iOS &amp; Android mobile apps</h5></a>
-            <p className="text-muted small-xl mb-2">Sep 27, 2018</p>
-            <p className="card-text">Nam liber tempor cum soluta nobis eleifend option congue nihil imper,
-              consectetur adipiscing elit. <a href="/">Learn more</a></p>
+        <div className='text-center h6'>CEO AND Founder</div>
+         <div className='text-center h3'>Samuel Anyanwutaku</div>
+         <p className='text-center'>Mc Master University, BA in Social and Philosophy</p>
           </div>
         </div>
       </div>
       <div className="col-md-4">
         <div className="card">
-          <a href="/">
-            <img className="card-img-top img-raised" src="img/blog-2.jpg" alt="Blog 2" />
+          <a href="/" >
+            <img className="card-img-top img-raised object" src={ASS} alt="Blog 2" />
           </a>
           <div className="card-body">
-            <a href="/" className="card-title mb-2"><h5>New update is available for the editor</h5></a>
-            <p className="text-muted small-xl mb-2">August 16, 2018</p>
-            <p className="card-text">Nam liber tempor cum soluta nobis eleifend option congue nihil imper,
-              consectetur adipiscing elit. <a href="/">Learn more</a></p>
+          <div className='text-center h6'>CFO  And Venture Panter</div>
+         <div className='text-center h3'>Chideraa Nwabasili</div>
+         <p className='text-center'>BSc. In Economics and Finance & Member of the Association of the Certified Chartered Accountants</p>
+            
           </div>
         </div>
       </div>
-      <div className="col-md-4">
-        <div className="card">
-          <a href="/">
-            <img className="card-img-top img-raised" src="img/blog-3.jpg" alt="Blog 3" />
-          </a>
-          <div className="card-body">
-            <a href="/" className="card-title mb-2"><h5>The story of building #1 page builder</h5></a>
-            <p className="text-muted small-xl mb-2">December 2nd, 2017</p>
-            <p className="card-text">Nam liber tempor cum soluta nobis eleifend option congue nihil imper,
-              consectetur adipiscing elit. <a href="/">Learn more</a></p>
-          </div>
-        </div>
-      </div>
+
     </div>
   
   </div>
-<div className="row mt-4">
+<div className="row mt-4" ref={ServicesRef}>
       <div className="col-md-6 mx-auto text-center">
         <h5 className="mb-4">Have questions?</h5>
         <a href="/" className="btn btn-primary">Contact us</a>
       </div>
+    </div>
+    <div className='justify mt-4'>
+      <div className='h5'>Telephone: <span className='text-muted'> +1 (437) 423 440</span></div>
+      <div className='h5'>Address: <span className='text-muted'>Toronto, Oranto, Canada</span></div>
     </div>
     <footer className="py-6">
   
@@ -217,23 +217,9 @@ function App() {
         <ul className="list-unstyled">
           <li><a href="/">Privacy</a></li>
           <li><a href="/">Terms</a></li>
-          <li><a href="/">Refund policy</a></li>
         </ul>
       </div>
-      <div className="col-sm-2">
-        <h5>Partner</h5>
-        <ul className="list-unstyled">
-          <li><a href="/">Refer a friend</a></li>
-          <li><a href="/">Affiliates</a></li>
-        </ul>
-      </div>
-      <div className="col-sm-2">
-        <h5>Help</h5>
-        <ul className="list-unstyled">
-          <li><a href="/">Support</a></li>
-          <li><a href="/">Log in</a></li>
-        </ul>
-      </div>
+    
     </div>
     <div className="row mt-5">
       <div className="col-12 text-muted text-center small-xl">
